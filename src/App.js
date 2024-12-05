1b03d9927844b334e98d6d1a2f311b1992eac321
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-12-05 18:22:39 +0000

    Added a logo to the header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
     </div>
   );
 }
-
-export default App;
