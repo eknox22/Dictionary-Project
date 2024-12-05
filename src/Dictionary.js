813b865e0670349007b7d216f9fa07ba7e6d5e7e
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-12-05 18:38:56 +0000

    Creating the input for the dictionary app

---
+++ src/Dictionary.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  function search(event) {
+    event.preventDefault();
+    alert("Searching...");
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} />
+      </form>
+    </div>
+  );
+}
