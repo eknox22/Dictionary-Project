8e1fdb9810853435bb8187370079b484bbdba12d
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-12-05 20:38:02 +0000

    Displaying the definition

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Meanings(props) {
-  return <div>Hello from Meaning</div>;
+  return (
+    <div className="meanings">
+      <h3>{props.meanings.partOfSpeech}</h3>
+      <p>{props.meanings.definition}</p>
+    </div>
+  );
 }
