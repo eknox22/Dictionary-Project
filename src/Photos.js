2e99b372f8b23052690a677af6eec0fed67a1022
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-12-07 11:58:30 +0000

    Updating photos compnent

--- src/Photos.js
+++ src/Photos.js
@@ -5,9 +5,22 @@ export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="Photos">
-        {props.photos.map(function (photo, index) {
-          return <img src={photo.src.landscape} key={index} />;
-        })}
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href="#" target="_blank">
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    rel="noreferrer"
+                    alt="photos"
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
       </section>
     );
   } else {
